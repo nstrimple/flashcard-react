ecb1ba191c4e17d6f6c90a44917a1e43f4e5731e
nstrimple <97196951+nstrimple@users.noreply.github.com>
2022-01-07 13:59:54 -0500

    Made the card flip actually work

--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react'
 import Card from '../UI/Card/Card';
 import classes from './Flashcard.module.css'
+import ReactCardFlip from 'react-card-flip';
 
 function Flashcard() {
     const front = 'Girl';
     const back = 'Menina';
-    const [ side, setSide ] = useState(true);
+    const [ side, setSide ] = useState(false);
     const flipHandler = () => {
         setSide((prevState) => {
             return !prevState
         })
     }
     return (
-        <div className={classes.CardFlip}>
-            <Card onClick={flipHandler}>{side ? front : back}</Card>
+        <div>
+            <ReactCardFlip isFlipped={side}>
+            <Card onClick={flipHandler}>{front}</Card>
+            <Card onClick={flipHandler}>{back}</Card>
+            </ReactCardFlip>
         </div>
     )
 }
