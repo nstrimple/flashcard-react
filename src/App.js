15641514a2df09927019ba094f811264ec63943c
nstrimple <97196951+nstrimple@users.noreply.github.com>
2022-01-07 00:32:14 -0500

    Creating the list of tasks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import react from "react";
 
 function App() {
-  return null;
+  return (
+    <>
+    <p>Create card component</p>
+    <p>Create flashcard scroll</p>
+    <p>Add flashcard api</p>
+    </>
+  );
 }
 
 export default App;
